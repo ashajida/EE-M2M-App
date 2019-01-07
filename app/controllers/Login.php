e12e08dc9297c8b4f4a4c31995c798d642e12ea9
ASHRAF K AJIDA <ashraf.ajida@yahoo.com>
2019-01-07 21:21:51 +0000

    working on user accounts

--- app/controllers/Login.php
+++ app/controllers/Login.php
@@ -1,20 +1,34 @@
 <?php
 
-class Login 
+/**
+ * Login Controller
+ * @author Ashraf Ajida
+ */
+
+
+require_once __DIR__ . '/Controller.php';
+
+
+class LoginController extends Controller
 {
-    private $view;
 
     public function __construct($container)
     {
-        $this->view = $container->get('view');
+        parent::__construct($container);
     }
 
     public function index($request, $response, $args) 
     {
 
         return $this->view->render($response, 'login.twig', [
-            'name' => 'ash'
+        
         ]);
+    }
+
+    public function login($request, $response, $args)
+    {
+        $username = filter_var($request->getParsed('username'), FILTER_SANITIZE_STRING);
+        $password = filter_var($request->getParsed('password'), FILTER_SANITIZE_STRING);
     }
 
 }
